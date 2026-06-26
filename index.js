69e0f759b157fd42b66f6123be7adc9d08c9e6d8
khalid66527 <khalidhasan678954321@gmail.com>
2026-06-26 23:40:03 +0600

    versel.json file and full project overview

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
+    
+    // await client.connect();
+    
     const database = client.db("b13_assignment_10_db");
     const artsCollection = database.collection("artstor");
     const usersCollection = database.collection("user");
@@ -561,7 +561,7 @@ async function run() {
     })
 
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
